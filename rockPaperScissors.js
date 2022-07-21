b82b7c1af303685889242a0b3628c0f9dfda077f
ebenezer.chelliah <egchelliah@gmail.com>
2022-07-20 23:40:20 -0600

    Get a single round of the game working

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -11,3 +11,44 @@ function getComputerChoice() {
 }
 
 const computerSelection = getComputerChoice();
+
+
+function playRound(playerSelection, computerSelection){
+    if (playerSelection !== 'rock' && 'paper' && 'scissors') {
+        console.log('INSIDE FUNCTION Players pick is ' + playerSelection);
+        console.log('INSIDE FUNCTION Computer pick is ' + computerSelection);
+        return console.log('You did not pick a valid option. The end')
+    }
+
+    else if (playerSelection === 'rock' && computerSelection === 'rock' ||
+    playerSelection === 'paper' && computerSelection === 'paper' ||
+    playerSelection === 'scissors' && computerSelection === 'scissors')
+    
+    {
+        console.log('INSIDE FUNCTION Players pick is ' + playerSelection);
+        console.log('INSIDE FUNCTION Computer pick is ' + computerSelection);
+        return console.log('You guys tied');
+
+    } else if (playerSelection === 'rock' && computerSelection === 'scissors' ||
+    playerSelection === 'paper' && computerSelection === 'rock' ||
+    playerSelection === 'scissors' && computerSelection === 'paper')
+    
+    {
+        console.log('INSIDE FUNCTION Players pick is ' + playerSelection);
+        console.log('INSIDE FUNCTION Computer pick is ' + computerSelection);
+        return console.log('The player won!')
+    
+
+    } else (playerSelection === 'rock' && computerSelection === 'paper' ||
+    playerSelection === 'paper' && computerSelection === 'scissors' ||
+    playerSelection === 'scissors' && computerSelection === 'rock')
+
+    {   
+        console.log('INSIDE FUNCTION Players pick is ' + playerSelection);
+        console.log('INSIDE FUNCTION Computer pick is ' + computerSelection);
+        return console.log ('The player lost')
+    }
+    
+}
+
+playRound(playerSelection, computerSelection );
